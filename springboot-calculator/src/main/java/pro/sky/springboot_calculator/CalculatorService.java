3e826305a5d8a0345fd2e3d15533f6aac32890ff
masha <mativaclub@gmail.com>
2023-04-14 17:43:01 +0300

    Commit Spring boot

---
+++ springboot-calculator/src/main/java/pro/sky/springboot_calculator/CalculatorService.java
@@ -0,0 +1,14 @@
+package pro.sky.springboot_calculator;
+
+public interface CalculatorService {
+
+    String welcome();
+
+    String plus(Integer num1, Integer num2);
+
+    String minus(Integer num1, Integer num2);
+
+    String multiply(Integer num1, Integer num2);
+
+    String divide(Integer num1, Integer num2);
+}
---
+++ springboot-calculator/src/main/java/pro/sky/springboot_calculator/SpringbootCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.springboot_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCalculatorApplication.class, args);
+	}
+
+}

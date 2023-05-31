537a618bb2798be38e06f18eedd2e3960da72907
masha <mativaclub@gmail.com>
2023-05-31 11:36:22 +0300

    All homeworks course 2

---
+++ spring_shoppingCart/src/test/java/pro/sky/spring_shoppingCart/SpringShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.spring_shoppingCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
